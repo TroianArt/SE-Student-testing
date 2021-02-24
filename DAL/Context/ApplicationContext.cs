cd0d13a5dd05bf0f1a7b4cb465a0183ee97ca407
Taras Vavrykovych <vavrycovych@gmail.com>
2021-02-24 16:59:24 +0200

    Create base architecture and create Identity Context class

---
+++ DAL/Context/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DAL.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@Environment.GetEnvironmentVariable("TestingDBConnectionString"));
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ApplicationContext context = new ApplicationContext();
+        }
+    }
+}
