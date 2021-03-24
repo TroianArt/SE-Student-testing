4faf537b3de15a9aa4f1d58034bc4ac881e95447
Taras Vavrykovych <vavrycovych@gmail.com>
2021-03-24 21:34:29 +0200

    Include Identity EF

--- student testing/Startup.cs
+++ student testing/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DAL.Context;
+using DAL.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace student_testing
 {
@@ -23,9 +26,14 @@ namespace student_testing
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
             services.AddControllersWithViews();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -44,6 +52,7 @@ namespace student_testing
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
