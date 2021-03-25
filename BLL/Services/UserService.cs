e5d3203edc1e14d91516db88fa47e7278bb81144
Taras Vavrykovych <vavrycovych@gmail.com>
2021-03-25 15:23:20 +0200

    Add User Service

---
+++ BLL/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using BLL.Interfaces;
+using DAL.Domain;
+using DAL.Interfaces;
+
+namespace BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+
+
+    }
+}
