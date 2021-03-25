7f70cfc296c8fd478c80cb481553b167a5a447f7
Nazar Kychma <Nazar Kychma@DESKTOP-BVOPMP4>
2021-03-25 16:20:55 +0200

    Add user sevice interface

--- BLL/Interfaces/IUserService.cs
+++ BLL/Interfaces/IUserService.cs
@@ -1,7 +1,15 @@
-﻿namespace BLL.Interfaces
+﻿using System.Threading.Tasks;
+using DAL.Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace BLL.Interfaces
 {
     public interface IUserService
     {
-        
+        Task<IdentityResult> SignUp(User user);
+
+        Task SignIn(User user, bool isPersistant);
+
+        Task<IdentityResult> ChangePassword(User user, string currentPassword, string newPassword);
     }
 }
--- BLL/Services/UserService.cs
+++ BLL/Services/UserService.cs
@@ -1,6 +1,8 @@
 ﻿using BLL.Interfaces;
 using DAL.Domain;
 using DAL.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace BLL.Services
 {
@@ -12,7 +14,19 @@ namespace BLL.Services
             this.unitOfWork = unitOfWork;
         }
 
+        public Task<IdentityResult> ChangePassword(User user, string currentPassword, string newPassword)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public Task SignIn(User user, bool isPersistant)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public Task<IdentityResult> SignUp(User user)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
