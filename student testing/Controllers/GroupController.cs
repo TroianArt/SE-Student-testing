136ddeb59be382ed1db86dd9e07d9fc441053251
Nazar Kychma <Nazar Kychma@DESKTOP-BVOPMP4>
2021-05-13 13:59:02 +0300

    Add ability to create group

--- student testing/Controllers/GroupController.cs
+++ student testing/Controllers/GroupController.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using student_testing.Models;
 using student_testing.Models.Group;
+using System.Security.Claims;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL.Domain;
+using DAL.Interfaces;
 
 namespace student_testing.Controllers
 {
     public class GroupController : Controller
     {
+        private IGroupService groupService;
+        private IUnitOfWork unitOfWork;
+
+        public GroupController(IGroupService service, IUnitOfWork uof)
+        {
+            this.groupService = service;
+            this.unitOfWork = uof;
+        }
+
         [HttpGet]
         [Authorize]
         public IActionResult Index() // returns all user groups
@@ -19,56 +33,69 @@ namespace student_testing.Controllers
         }
 
         [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Create(CreateGroupViewModel model) //create new group
+        [Authorize(Roles = "Teacher")]
+        public async Task<IActionResult> Create(CreateGroupViewModel model) //create new group
         {
+            if (this.ModelState.IsValid)
+            {
+                User user = await unitOfWork.UserManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+                GroupDto groupDto = new GroupDto
+                {
+                    DateCreation = DateTime.Now,
+                    Name = model.Name,
+                    Description = model.Description,
+                    CreatorId = user.Id,
+                };
+                await groupService.CreateGroup(groupDto);
+                return View();
+            }
             return View();
         }
 
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Teacher")]
         public IActionResult Create() //create new group
         {
             return View();
         }
 
         [HttpPut]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Teacher")]
         public IActionResult Update(UpdateGroupViewModel model) //update existed group
         {
             return View();
         }
 
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Teacher")]
         public IActionResult Update() //update existed group
         {
             return View();
         }
 
         [HttpPost]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Teacher")]
         public IActionResult AddStudent(AddStudentToGroupViewModel model) //add student to group
         {
             return View();
         }
 
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Teacher")]
         public IActionResult AddStudent() //add student to group
         {
             return View();
         }
 
         [HttpDelete]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Teacher")]
         public IActionResult Delete(int id) //delete student from group
         {
             return View();
         }
 
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Teacher")]
         public IActionResult Delete() //delete student from group
         {
             return View();
