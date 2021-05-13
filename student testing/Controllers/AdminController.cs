c016814ac2e3cb825e85885961498e1d89909ccf
Северин Янчинський <yanchynskyjs@gmail.com>
2021-05-13 16:35:15 +0300

    admin page

--- student testing/Controllers/AdminController.cs
+++ student testing/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using student_testing.Models.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +8,16 @@ using System.Threading.Tasks;
 
 namespace student_testing.Controllers
 {
+   
     public class AdminController : Controller
     {
+        Dictionary<string, string> d = new Dictionary<string, string> { { "test@gmail.com", "Student" }, { "teacher@gmail.com", "Teacher" } };
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
-            return View();
+            AdminPageModel am = new AdminPageModel { Users = d };
+            return View(am);
         }
 
         //[HttpPost]
---
+++ student testing/Models/Admin/AdminPageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace student_testing.Models.Admin
+{
+
+    public class AdminPageModel
+    {
+        [Required]
+        public Dictionary<string, string> Users { get; set; }
+
+    }
+}
