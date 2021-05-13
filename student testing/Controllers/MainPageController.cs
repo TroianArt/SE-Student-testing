abc2e00a2fe84aef1ac563e855a7af99596dedb2
Северин Янчинський <yanchynskyjs@gmail.com>
2021-05-13 16:07:25 +0300

    add group

--- student testing/Controllers/MainPageController.cs
+++ student testing/Controllers/MainPageController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using student_testing.Models.MainPage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,16 @@ namespace student_testing.Controllers
 {
     public class MainPageController : Controller
     {
+        List<DAL.Domain.Group> l = new List<DAL.Domain.Group> ();
 
         [Authorize]
         [HttpGet]
         public IActionResult Main()
         {
-            return this.View();
+            DAL.Domain.Group ng = new DAL.Domain.Group { CreatorId = "1", Name = "test", DateCreation =DateTime.Now, Description = "Loh"};
+            l.Add(ng);
+            MainModel mm = new MainModel { Classes = l};
+            return this.View(mm);
         }
     }
 }
---
+++ student testing/Models/MainPage/MainModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL.Domain;
+namespace student_testing.Models.MainPage
+{
+    public class MainModel
+    {
+
+        public List<DAL.Domain.Group> Classes { get; set; }
+
+        
+    }
+}
